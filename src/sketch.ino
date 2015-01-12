d7a1d61030430a29c53af26637dc0d75fbde130a
Anders Andersson <git@panic.nu>
2015-01-12 21:40:44 +0100

    - Button now turns the lighthouse off, nothing more. - Uses a RGB LED on pins A0-A2 - Lighthouse Tistlarna added

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,8 +1,14 @@
-const int LED_PIN = 13;
+const int RED_PIN = 14;
+const int GREEN_PIN = 15;
+const int BLUE_PIN = 16;
 const int BTN_PIN = 2;
-const int CMD_LENGTH = 33;
 
-int isOn = false;
+const int RED = 0;
+const int GREEN = 1;
+const int WHITE = 2;
+const int BLACK = 3;
+
+
 int btnState = HIGH;
 int oldBtnState = HIGH;
 
@@ -14,15 +20,36 @@ void toggleBtn() {
   btnState = digitalRead(BTN_PIN);
 
   if (btnState == HIGH && oldBtnState == LOW) {
-    isOn = ! isOn;
+    command = "off";
     delay(10);
   }
 
 }
 
+void writeColor(unsigned int red, unsigned int green, unsigned blue) {
+  analogWrite(RED_PIN, red);
+  analogWrite(BLUE_PIN, green);
+  analogWrite(GREEN_PIN, blue);
+}
+
+void writeFixedColor(unsigned int color) {
+  if (color == RED) {
+    writeColor(255,0,0);
+  } else if (color == GREEN) {
+    writeColor(0,255,0);
+  } else if (color == WHITE) {
+    writeColor(255,255,255);
+  } else {
+    writeColor(0,0,0);
+  }
+}
+
+
 void setup()
 {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
   pinMode(BTN_PIN, INPUT);
 
   Serial.begin(9600);
@@ -34,9 +61,23 @@ void vinga_ostra_nedre() {
   int modTime = time % 4000;
 
   if (modTime < 500) {
-    digitalWrite(LED_PIN, LOW);
+    writeFixedColor(BLACK);
   } else {
-    digitalWrite(LED_PIN, HIGH);
+    writeFixedColor(WHITE);
+  }
+}
+
+void tistlarna() {
+  int modTime = time % 15000;
+
+  if (modTime < 2000) {
+    writeFixedColor(WHITE);
+  } else if (modTime < 8000) {
+    writeFixedColor(BLACK);
+  } else if (modTime < 10000) {
+    writeFixedColor(WHITE);
+  } else {
+    writeFixedColor(BLACK);
   }
 }
 
@@ -53,8 +94,10 @@ void loop()
 
   if (command == "VINGA_OSTRA_NEDRE") {
     vinga_ostra_nedre();
+  } else if (command == "TISTLARNA") {
+    tistlarna();
   } else {
-    digitalWrite(LED_PIN, LOW);
+    writeColor(0,0,0);
   }
 
 }
